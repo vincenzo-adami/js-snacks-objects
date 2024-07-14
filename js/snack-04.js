cbb9c1e2e84fb63d4b27eb341f5cd5d456ad5bf9
vincenzo-adami <vincenzoadami92@gmail.com>
2024-07-14 15:57:09 +0200

    ADD: loop create new array with result

--- js/snack-04.js
+++ js/snack-04.js
@@ -1,9 +1,23 @@
 let persone = [
-  { nome: 'Mario', cognome: 'Rossi', eta: '25' },
-  { nome: 'Maria', cognome: 'Bianchi', eta: '17' },
-  { nome: 'Antonio', cognome: 'Verdi', eta: '48' },
-  { nome: 'Francesca', cognome: 'Abate', eta: '18' },
-  { nome: 'Anna', cognome: 'Guido', eta: '15' },
-  { nome: 'Giuseppe', cognome: 'Franchi', eta: '68' },
-  { nome: 'Elena', cognome: 'Pace', eta: '37' }
+  { nome: 'Mario', cognome: 'Rossi', eta: 25 },
+  { nome: 'Maria', cognome: 'Bianchi', eta: 17 },
+  { nome: 'Antonio', cognome: 'Verdi', eta: 48 },
+  { nome: 'Francesca', cognome: 'Abate', eta: 18 },
+  { nome: 'Anna', cognome: 'Guido', eta: 15 },
+  { nome: 'Giuseppe', cognome: 'Franchi', eta: 68 },
+  { nome: 'Elena', cognome: 'Pace', eta: 37 }
 ]
+
+console.log(persone)
+
+let risultato = [];
+
+for (let i = 0; i < persone.length; i++) {
+  if (persone[i].eta < 18) {
+    risultato.push(persone[i].nome + ' ' + persone[i].cognome + ' non può ancora guidare')
+  } else {
+    risultato.push(persone[i].nome + ' ' + persone[i].cognome + ' può guidare')
+  }
+}
+
+console.log(risultato)

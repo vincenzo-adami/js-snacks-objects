bf605041db8cdb57534974b54795fa800a31f106
vincenzo-adami <vincenzoadami92@gmail.com>
2024-07-14 15:23:43 +0200

    ADD: created, add base array of object to be filtered

---
+++ js/snack-o3.js
@@ -0,0 +1,6 @@
+let animali = [
+  { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+  { nome: 'coccodrillo', famiglia: 'sauropsidi', classe: 'rettili' }
+]

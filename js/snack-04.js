1c4e7d1ee5321b3216e25dda0686aaf64ee2d6fd
vincenzo-adami <vincenzoadami92@gmail.com>
2024-07-14 15:44:59 +0200

    ADD: create file, add array of objects

---
+++ js/snack-04.js
@@ -0,0 +1,9 @@
+let persone = [
+  { nome: 'Mario', cognome: 'Rossi', eta: '25' },
+  { nome: 'Maria', cognome: 'Bianchi', eta: '17' },
+  { nome: 'Antonio', cognome: 'Verdi', eta: '48' },
+  { nome: 'Francesca', cognome: 'Abate', eta: '18' },
+  { nome: 'Anna', cognome: 'Guido', eta: '15' },
+  { nome: 'Giuseppe', cognome: 'Franchi', eta: '68' },
+  { nome: 'Elena', cognome: 'Pace', eta: '37' }
+]
